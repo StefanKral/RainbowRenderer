b944113564b856de8fec8c864f273ddba76da83b
StefanKral <stefan.kral@aon.at>
2024-04-07 18:15:28 +0200

    Ruff Formatting

--- test_RendererFunctions.py
+++ test_RendererFunctions.py
@@ -1,15 +1,18 @@
 import RainbowRendererMain
 
+
 def test_StandardScreenSizes():
     assert RainbowRendererMain.getStandardScreenDefinitions("landscape", "HD") == (1920, 1080)
     assert RainbowRendererMain.getStandardScreenDefinitions("portrait", "HD") == (1080, 1920)
     assert RainbowRendererMain.getStandardScreenDefinitions("fail", "HD") == (0, 0)
+
 
 def test_normalizedPixelPositions():
     assert RainbowRendererMain.getNormalizedPixelPositions((100, 100))["x"][0] == 0
     assert RainbowRendererMain.getNormalizedPixelPositions((100, 100))["x"][99] == 1.0
     assert RainbowRendererMain.getNormalizedPixelPositions((100, 100))["y"][0] == 0
     assert RainbowRendererMain.getNormalizedPixelPositions((100, 100))["y"][99] == 1.0
+
 
 def test_normalizeTo8Bit():
     assert RainbowRendererMain.normalizeTo8Bit(256) == 255
