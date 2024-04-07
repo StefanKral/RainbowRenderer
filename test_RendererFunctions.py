091dc10156336c19248b69e014ddcb6aceaf6c88
StefanKral <stefan.kral@aon.at>
2024-04-07 18:49:23 +0200

    Fixed normalizeTo8Bit Testing

--- test_RendererFunctions.py
+++ test_RendererFunctions.py
@@ -20,7 +20,7 @@ def test_normalizedPixelPositions():
 
 
 def test_normalizeTo8Bit():
-    assert RainbowRendererMain.normalizeTo8Bit(1.012) == 254
+    assert RainbowRendererMain.normalizeTo8Bit(1.012) == 255
     assert RainbowRendererMain.normalizeTo8Bit(264.4312) == 255
     assert RainbowRendererMain.normalizeTo8Bit(0.998) == 254
     assert RainbowRendererMain.normalizeTo8Bit(-0.25) == 0
